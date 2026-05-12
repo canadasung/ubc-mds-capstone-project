9342cd303e344ab606d83cb52a947c87d8505aa1
Molly Kessler <88510334+kessler24@users.noreply.github.com>
2026-05-12 10:16:15 -0700

    Change fixture scope to 'class' for tests

--- tests/APIs/test_BryophytePortal.py
+++ tests/APIs/test_BryophytePortal.py
@@ -18,15 +18,15 @@ from test_API import ApiContractTests
 
 
 class TestBryophytePortalContract(ApiContractTests):
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     def api_fn(self):
         return get_bryophyteportal_synonyms
 
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     def valid_species_with_synonyms(self):
         return "Marchantia polymorpha"
 
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     def valid_species_no_synonyms(self):
         return "Fissidens aphelotaxifolius"
 
